51fc005640261ca7d2973a31e82cd3dce424b21f
RiccardoNestola <riccardinux@gmail.com>
2022-11-25 16:11:38 +0100

    snack 2

---
+++ snack 2/js/script.js
@@ -0,0 +1,16 @@
+
+
+/* Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo. */
+
+
+
+  let form =  parseInt( prompt("Inserisci un numero"));
+
+  if (form % 2 == 0 ) {
+    console.log(contenitore);
+    
+  } else {
+
+    console.log(contenitore + 1)
+  }
+
