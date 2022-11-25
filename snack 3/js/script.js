84c890e66cd6b16eecebd9d102059c39f54e046f
RiccardoNestola <riccardinux@gmail.com>
2022-11-25 16:44:16 +0100

    add

---
+++ snack 3/js/script.js
@@ -0,0 +1,17 @@
+
+
+/* Calcola la somma dei primi 10 numeri di un array. */
+
+
+
+  const lista =  [10,10,43,2,3,4,6,7,8,,7,2,4,5,6,7,8];
+
+  let sum = 0;
+
+  for (let i = 0; i < 10; i++) {
+    
+    sum =+ lista[i];
+    console.log(sum);
+}
+
+
