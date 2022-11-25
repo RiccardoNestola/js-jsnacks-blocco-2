9d00d7f3ffc82c6ec0a23c51af79d1c30eeae374
RiccardoNestola <riccardinux@gmail.com>
2022-11-25 17:05:11 +0100

    bugfix

--- snack 3/js/script.js
+++ snack 3/js/script.js
@@ -4,19 +4,20 @@
 
 
 
-  const lista =  [10,10,43,2,3,4,6,7,8,,7,2,4,5,6,7,8];
+  const lista =  [10,10,43,2,3,4,6,7,8,7,2,4,5,6,7,8];
 
   let sum = 0;
 
+  let average = 0;
+
   for (let i = 0; i < 10; i++) {
     
-    sum =+ lista[i];
-    sum =+ (lista / lista[1]);
-    
+    sum =+ lista[1];
+    average = sum / 10;
 
     
 }
 
-console.log(sum);
-console.log(sum);
 
+console.log(sum);
+console.log(average);
