00022f2ee4ba96fa6b74b6e1ebf1301b8fe57668
RiccardoNestola <riccardinux@gmail.com>
2022-11-28 12:47:53 +0100

    fix

--- snack 6/js/script.js
+++ snack 6/js/script.js
@@ -7,11 +7,11 @@
 
 let contenitore = [1,4,5,3,2,4,5,6,8,9,9,7,10,82];
 
-
+let sum =  0;
 
 for (let index = 0; index < contenitore.length; index++) {
 
-  let sum =  0;
+  
 
   if (contenitore[index] % 2 == 1 ) {
     sum += contenitore[index]
